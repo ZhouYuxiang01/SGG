4f22e968a06d0b5e460855537e181a067c212d5d
Hastur0451 <zy20020220@gmail.com>
2024-10-05 17:47:14 +1000

    update question function

---
+++ SGG/Assets/AnswerSystem/Scripts/Answer.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Answer : MonoBehaviour
+{
+    // 题目数据
+    private string[][] ArrayX;
+    private string[] lineArray;
+    private int topicMax = 0;
+    private List<bool> isAnserList = new List<bool>();
+
+    // UI 元素
+    public GameObject tipsbtn;
+    public TextMeshProUGUI tipsText;
+    public List<Toggle> toggleList;
+    public TextMeshProUGUI indexText;
+    public TextMeshProUGUI TM_Text;
+    public List<TextMeshProUGUI> DA_TextList;
+
+    // 按钮
+    public Button BtnBack;
+    public Button BtnNext;
+    public Button BtnTip;
+
+    // 统计信息
+    public TextMeshProUGUI TextAccuracy;
+    private int topicIndex = 0;
+    private int anserint = 0;
+    private int isRightNum = 0;
+
+    private void Start()
+    {
+        SetupListeners();
+    }
+
+    private void SetupListeners()
+    {
+        for (int i = 0; i < toggleList.Count; i++)
+        {
+            int index = i;
+            toggleList[i].onValueChanged.AddListener((isOn) => AnswerRightWrongJudgment(isOn, index));
+        }
+
+        BtnTip.onClick.AddListener(() => Select_Answer(0));
+        BtnBack.onClick.AddListener(() => Select_Answer(1));
+        BtnNext.onClick.AddListener(() => Select_Answer(2));
+    }
+
+    public void InitializeQuiz(string quizText)
+    {
+        ParseQuizText(quizText);
+        ResetQuiz();
+        LoadAnswer();
+    }
+
+    private void ParseQuizText(string quizText)
+    {
+        lineArray = quizText.Split('\n');
+        ArrayX = new string[lineArray.Length][];
+        for (int i = 0; i < lineArray.Length; i++)
+        {
+            ArrayX[i] = lineArray[i].Split(':');
+        }
+        topicMax = lineArray.Length;
+        isAnserList.Clear();
+        for (int x = 0; x < topicMax; x++)
+        {
+            isAnserList.Add(false);
+        }
+    }
+
+    private void ResetQuiz()
+    {
+        topicIndex = 0;
+        anserint = 0;
+        isRightNum = 0;
+        TextAccuracy.text = "Accuracy 0%";
+    }
+
+    private void LoadAnswer()
+    {
+        ResetToggles();
+        ResetTips();
+        UpdateQuestionDisplay();
+    }
+
+    private void ResetToggles()
+    {
+        foreach (var toggle in toggleList)
+        {
+            toggle.isOn = false;
+            toggle.interactable = true;
+        }
+    }
+
+    private void ResetTips()
+    {
+        tipsbtn.SetActive(false);
+        tipsText.text = "";
+    }
+
+    private void UpdateQuestionDisplay()
+    {
+        indexText.text = "Question " + (topicIndex + 1);
+        TM_Text.text = ArrayX[topicIndex][1];
+        int optionCount = ArrayX[topicIndex].Length - 3;
+        
+        for (int x = 0; x < DA_TextList.Count; x++)
+        {
+            if (x < optionCount)
+            {
+                DA_TextList[x].text = ArrayX[topicIndex][x + 2];
+                DA_TextList[x].transform.parent.gameObject.SetActive(true);
+            }
+            else
+            {
+                DA_TextList[x].transform.parent.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void Select_Answer(int index)
+    {
+        switch (index)
+        {
+            case 0: // 提示
+                ShowTip();
+                break;
+            case 1: // 上一题
+                NavigateToPreviousQuestion();
+                break;
+            case 2: // 下一题
+                NavigateToNextQuestion();
+                break;
+        }
+    }
+
+    private void ShowTip()
+    {
+        int correctAnswerIndex = int.Parse(ArrayX[topicIndex][ArrayX[topicIndex].Length - 1]) - 1;
+        string correctAnswerLetter = "ABCD"[correctAnswerIndex].ToString();
+        tipsText.text = $"<color=#FFAB08FF>Correct Answer is {correctAnswerLetter}</color>";
+    }
+
+    private void NavigateToPreviousQuestion()
+    {
+        if (topicIndex > 0)
+        {
+            topicIndex--;
+            LoadAnswer();
+        }
+        else
+        {
+            tipsText.text = "<color=#27FF02FF>Not more question before</color>";
+        }
+    }
+
+    private void NavigateToNextQuestion()
+    {
+        if (topicIndex < topicMax - 1)
+        {
+            topicIndex++;
+            LoadAnswer();
+        }
+        else
+        {
+            tipsText.text = "<color=#27FF02FF>Already is last one</color>";
+        }
+    }
+
+    private void AnswerRightWrongJudgment(bool isOn, int index)
+    {
+        if (!isOn) return;
+
+        int correctAnswerIndex = int.Parse(ArrayX[topicIndex][ArrayX[topicIndex].Length - 1]) - 1;
+        bool isCorrect = (index == correctAnswerIndex);
+
+        UpdateTipsForAnswer(isCorrect);
+        UpdateAccuracy(isCorrect);
+        DisableToggles();
+    }
+
+    private void UpdateTipsForAnswer(bool isCorrect)
+    {
+        tipsText.text = isCorrect ? "<color=#27FF02FF>Correct!</color>" : "<color=#FF0020FF>Sorry, wrong answer</color>";
+        tipsbtn.SetActive(true);
+    }
+
+    private void UpdateAccuracy(bool isCorrect)
+    {
+        if (!isAnserList[topicIndex])
+        {
+            anserint++;
+            if (isCorrect) isRightNum++;
+            isAnserList[topicIndex] = true;
+            TextAccuracy.text = $"Accuracy {((float)isRightNum / anserint * 100):F2}%";
+        }
+        else
+        {
+            tipsText.text = "<color=#FF0020FF>Already answered</color>";
+        }
+    }
+
+    private void DisableToggles()
+    {
+        foreach (var toggle in toggleList)
+        {
+            toggle.interactable = false;
+        }
+    }
+}
--- SGG/Assets/Yao/Scrpite/Quiz Manager.cs
+++ SGG/Assets/Yao/Scrpite/Quiz Manager.cs
@@ -3,42 +3,67 @@ using System.Collections.Generic;
 
 public class QuizManager : MonoBehaviour
 {
+    public static QuizManager Instance { get; private set; }
+
     [System.Serializable]
     public class PlantQuiz
     {
         public string plantName;
-        public string quizFileName;
+        public TextAsset quizTextAsset;
     }
 
     public List<PlantQuiz> plantQuizzes = new List<PlantQuiz>();
-    public Answer answerScript;
+    private Answer answerScript;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
-        // 确保 Answer 脚本被正确引用
-        if (answerScript == null)
+        string selectedPlant = PlayerPrefs.GetString("SelectedPlant", "");
+        if (!string.IsNullOrEmpty(selectedPlant))
         {
-            answerScript = FindObjectOfType<Answer>();
-            if (answerScript == null)
-            {
-                Debug.LogError("Answer script not found!");
-                return;
-            }
+            LoadQuizForPlant(selectedPlant);
         }
     }
 
     public void LoadQuizForPlant(string plantName)
     {
-        PlantQuiz quiz = plantQuizzes.Find(q => q.plantName == plantName);
-        if (quiz != null)
+        if (answerScript == null)
         {
-            answerScript.LoadQuizFromFile(quiz.quizFileName);
+            answerScript = FindObjectOfType<Answer>();
+        }
+
+        if (answerScript == null)
+        {
+            Debug.LogError("Answer script not found in the scene!");
+            return;
+        }
+
+        PlantQuiz quiz = plantQuizzes.Find(q => q.plantName == plantName);
+        if (quiz != null && quiz.quizTextAsset != null)
+        {
+            answerScript.InitializeQuiz(quiz.quizTextAsset.text);
         }
         else
         {
-            Debug.LogError($"Quiz for plant {plantName} not found!");
+            Debug.LogError($"Quiz for plant {plantName} not found or text asset is missing!");
         }
     }
 
-    // 可以添加更多方法来管理题库，如添加新的植物题库、移除题库等
+    public TextAsset GetQuizTextAsset(string plantName)
+    {
+        PlantQuiz quiz = plantQuizzes.Find(q => q.plantName == plantName);
+        return quiz?.quizTextAsset;
+    }
 }
